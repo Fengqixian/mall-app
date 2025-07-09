52a692607dc5019d6e574ddcbd5787776583e0f6
lsl <632>
2025-07-09 16:12:43 +0800

    first commit

--- src/pages/webview/webview.vue
+++ src/pages/webview/webview.vue
@@ -11,7 +11,7 @@
 	const lang = ref('th');
 	 lang.value =uni.getStorageSync('appLanguage').split('-')[0]
 	 console.log(lang.value,'lang.value')
-	const url = ref(`http://192.168.101.7:5173/h5/src/googleMaps/index.html?lat=${lat.value}&lng=${lng.value}&lang=${lang.value}`);
+	const url = ref(`https://freshdala-555.com/h5/hybrid/html/index.html?lat=${lat.value}&lng=${lng.value}&lang=${lang.value}`);
 
 	const onWebViewMessage = (e) => {
 		console.log(e.detail.data[0]);
